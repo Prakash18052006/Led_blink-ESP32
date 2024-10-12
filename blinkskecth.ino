7442d776fa3eb9741695e846ef88c858b22bf8f8
Prakash Choudhary <162735839+Prakash18052006@users.noreply.github.com>
2024-10-12 15:50:40 +0530

    ADD

---
+++ blinkskecth.ino
@@ -0,0 +1,10 @@
+int ledPin = 2;
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+void loop() {
+  digitalWrite(ledPin, HIGH);
+  delay(100);
+  digitalWrite(ledPin, LOW);
+  delay(100);
+}
---
+++ led_blink_sketch.ino
@@ -0,0 +1,15 @@
+#define led 2
+void setup() {
+  pinMode(led, OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite (led, HIGH);
+  delay(1000);
+  digitalWrite (led, LOW);
+  delay(1000);
+  // put your main code here, to run repeatedly:
+
+}
